b5df5c5032c8cac6a572fc3cde5521a71ff0020e
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-01-06 18:17:35 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(
   cors({
     // origin: ["http://localhost:5173"],
-    origin: ["https://fish-aquarium-blue.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"]
   })
 )
